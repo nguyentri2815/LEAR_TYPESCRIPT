40b265569d2e90f038384d23eeb8a22b430e8753
Tri Nguyen <tri.nguyen@alpaca.vn>
2026-04-03 17:54:47 +0700

    TS-intermediate-day-3-zod valid dữ liệu chắc chắn hơn

--- app/appointments/schema.ts
+++ app/appointments/schema.ts
@@ -1,10 +1,10 @@
 import z from "zod";
 
 export const createAppointmentSchema = z.object({
-  title: z.string().min(1, "Title is required"),
-  customerName: z.string().min(1, "Customer name is required"),
-  fee: z.number().min(0, "Fee is required"),
-  note: z.string().optional(),
+  title: z.string().trim().min(1, "Title is required"),
+  customerName: z.string().trim().min(1, "Customer name is required"),
+  fee: z.number().refine((val) => !isNaN(val), "Fee is must be a number").min(1, "Fee is required"),
+  note: z.string().trim().optional(),
 });
 
 export type CreateAppointmentSchemaValues = z.infer<typeof createAppointmentSchema>;
